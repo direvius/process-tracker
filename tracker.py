8e11eb0132e939d13dde43d1a5c2a1d497db8161
Alexey Lavrenuke <direvius@gmail.com>
2012-08-22 13:32:59 +0400

    extract class ProcessTracker

--- tracker.py
+++ tracker.py
@@ -13,7 +13,22 @@ logging.basicConfig(level=logging.INFO,
 #                    filename=opts.log,
 #                    filemode='w'
 					)
+
 class ProcessTracker:
+	def __init__(self, pid, name):
+		self.process = psutil.Process(int(pid))
+		self.name = name
+	def collect(self):
+		results = {}
+		try:
+			p = self.process
+			results["%s.cpu.pct"%self.name] = p.get_cpu_percent(0)
+			results["%s.cpu.usr"%self.name], results["%s.cpu.sys"%self.name] = p.get_cpu_times()
+			results["%s.csw.voluntary"%self.name], results["%s.csw.involuntary"%self.name] = p.get_num_ctx_switches()
+		except psutil.NoSuchProcess:
+			logging.exception("No such process")
+		return results
+class TrackerManager:
 	def __init__(self):
 		self.pids = {}
 		self.pidnames = {}
@@ -21,33 +36,22 @@ class ProcessTracker:
 		self.scheduler.add_interval_job(self.tracking_job, seconds=1)
 		self.scheduler.start()
 	def track(self, pid, name):
-		self.pids[pid] = psutil.Process(int(pid))
-		self.pidnames[pid] = name
+		self.pids[pid] = ProcessTracker(pid, name)
 	def untrack(self, pid):
 		try:
 			del self.pids[pid]
-			del self.pidnames[pid]
 		except KeyError:
 			logging.warning("PID %s was not tracked" % pid)
 	def tracking_job(self):
 		print "Tracked pids: %s\n" % ' '.join(self.pids)
 		for pid in self.pids.keys():
-			self.submit(pid,self.collect(pid))
-	def collect(self, pid):
-		results = {}
-		try:
-			p = self.pids[pid]
-			results["cpu.pct"] = p.get_cpu_percent(0)
-			results["cpu.usr"], results["cpu.sys"] = p.get_cpu_times()
-			results["csw.voluntary"], results["csw.involuntary"] = p.get_num_ctx_switches()
-		except psutil.NoSuchProcess:
-			logging.exception("No such process")
-		return results
+			self.submit(pid,self.pids[pid].collect())
+
 	def submit(self, pid, results):
 		for metric in results.keys():
-			logging.info("%s.%s\t%s\t%d" % (self.pidnames[pid], metric, results[metric], time.time()))
+			logging.info("%s\t%s\t%d" % (metric, results[metric], time.time()))
 def main():
-	pt = ProcessTracker()
+	tm = TrackerManager()
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.bind(("0.0.0.0",6666))
 	s.listen(1)
@@ -56,18 +60,19 @@ def main():
 		data = conn.recv(1024)
 		if not data: break
 		# TODO: better cmd parser logic
+		# TODO: multithreaded managing console
 		try:
 			command, pid, name = data.rstrip('\r\n').split(' ')
 			if command == 'track':
-				pt.track(pid, name)
+				tm.track(pid, name)
 				conn.sendall('OK\n')
 			elif command == 'untrack':
-				pt.untrack(pid)
+				tm.untrack(pid)
 				conn.sendall('OK\n')
 			else:
 				conn.sendall('FAIL\n')
 		except ValueError:
-			conn.sendall('FAIL\n**USAGE:\n\ttrack <pid> <key> -- to start tracking pid\n\tuntrack <pid> -- to stop tracking pid\n')
+			conn.sendall('FAIL\n**USAGE:\n\ttrack <pid> <key> -- to start tracking pid\n\tuntrack <pid> ! -- to stop tracking pid\n')
 	conn.close()
 
 if __name__ == "__main__":
